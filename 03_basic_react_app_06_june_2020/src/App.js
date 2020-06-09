84934ff47790716812935d008f470bbd000ea39e
Danish Riaz <danishriaz0609@outlook.com>
2020-06-09 16:29:49 +0500

    03_basic_react_app_06_june_2020

---
+++ 03_basic_react_app_06_june_2020/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './main.css'
+
+function App() {
+  return (
+    <div className="center">
+      Hello it's me <strong>Danish Riaz</strong>
+      <p className="card">Inserting some javascript 10 + 5 = {10 + 5}</p>
+    </div>
+  );
+}
+
+export default App;
